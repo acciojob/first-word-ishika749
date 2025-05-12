1213e651729a1873c95374a0f012731bbb29f459
ishika749 <ishikashah749@gmail.com>
2025-05-12 11:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Handle empty string case
+    if (str.length === 0) return '';
+    
+    // Find index of first space
+    const spaceIndex = str.indexOf(' ');
+    
+    // If no space found, return entire string
+    if (spaceIndex === -1) return str;
+    
+    // Return substring up to first space
+    return str.substring(0, spaceIndex);
+}
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
